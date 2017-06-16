ffe7130d131cca24d9a9a946fae8f70f5f6b607f
Darek P <dajmos007@gmail.com>
2017-06-16 16:38:58 +0200

    Zmianki w kodzie systemu..

--- src/se/webbfox/Main.java
+++ src/se/webbfox/Main.java
@@ -1,5 +1,7 @@
 package se.webbfox;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +12,30 @@ public class Main {
 //        • do 85.528 podatek wynosi 18% podstawy minus 556,02 PLN,
 //        • od 85.528 podatek wynosi 14.839,02 zł + 32% nadwyżki ponad 85.528,00
 //
+        double podatek;
+        double zmiennaMinus= -556.02;
+        double zmienna1= 85.528;
+        System.out.println("Podaj informacje o swoim podatku ");
+
+
+        Scanner in= new Scanner(System.in);
+        double dochod=in.nextInt();
+
+        if (zmienna1 <= dochod){
+
+            double cena1= 18% + zmiennaMinus;
+            double final1= dochod+cena1;
+
+            System.out.println("Wartosc podatku to "+ final1);
+
+
+
+        }
+
+
+
+
+
 
 
 
